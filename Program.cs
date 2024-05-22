638a714320679e0ecf618b7d9307ef86255421f7
alidhm <73141640+alidhm@users.noreply.github.com>
2024-05-22 12:26:19 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Number 1: ");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.Write("Please Enter Number 2: ");
+            float num2 = float.Parse(Console.ReadLine());
+            float sum = num1 + num2;
+            float avg = sum / 2;
+            if (avg > num1)
+            {
+                Console.WriteLine("Yes!");
+            }
+            else
+            {
+                Console.WriteLine("No!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
